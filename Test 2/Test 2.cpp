aef825ffdaa2b3391d47ba8a9e0ab24934f2d089
JediForceD1 <44473978+JediForceD1@users.noreply.github.com>
2019-03-11 13:24:04 -0500

    Finish

--- Test 2/Test 2.cpp
+++ Test 2/Test 2.cpp
@@ -1,21 +1,65 @@
-// Test 2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+/*
+Assignment1ACM.cpp
+Aaron McCormick
+Calculating taxes for a given number
+*/
 #include "pch.h"
 #include <iostream>
+#include <iomanip>
+#include <limits>
+#include <string>
+#include <stdio.h> 
+#include <math.h>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+void print_numbers(int intnum_1, int intnum_2, int intnum_3);
+float get_average_value(int intnum_1, int intnum_2, int intnum_3);
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	srand(time(NULL));
+	int intnum_1, intnum_2, intnum_3, dump;
+	float avg_value;
+
+	//Write code to place number as per specifications into intnum_1
+	do {
+		
+
+		cout << "Please entere a number that is a multiple of 5 and between 1 - 30" << endl;
+		cin >> intnum_1;
+
+		cin.clear();
+		cin.ignore(100, '\n');
+		
+		dump = intnum_1 % 5;
+
+	} while (intnum_1 < 1 || intnum_1 > 30 || cin.fail() || dump > 0);
+
+	intnum_2 = rand() % 21;
+	intnum_3 = rand() % 21;
+
+	print_numbers(intnum_1, intnum_2, intnum_3);
+	avg_value = get_average_value(intnum_1, intnum_2, intnum_3);
+
+	cout << fixed << setprecision(1);
+	cout << "Average value = " << avg_value << endl;
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void print_numbers(int intnum_1, int intnum_2, int intnum_3) {
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	cout << "The first number is " << intnum_1 << " Your second number is " << intnum_2 << " Your third number is " << intnum_3 << "." << endl;
+
+}
+
+float get_average_value(int intnum_1, int intnum_2, int intnum_3) {
+
+	float average;
+
+	average = (intnum_1 + intnum_2 + intnum_3) / 3;
+
+	return average;
+
+}
